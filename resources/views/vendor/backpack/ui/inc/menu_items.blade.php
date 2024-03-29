57c9d2d6e12f151d3b00f28165ae752dba95bb4b
Luka Nikitin <x0333@pm.me>
2024-03-29 21:49:20 +0300

    laravel backpack

---
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -0,0 +1,5 @@
+{{-- This file is used for menu items by any Backpack v6 theme --}}
+
+<x-backpack::menu-item title="Orders" icon="la la-question" :link="backpack_url('order')" />
+<x-backpack::menu-item title="Consultations" icon="la la-question" :link="backpack_url('consultation')" />
+<x-backpack::menu-item title="Mailings" icon="la la-question" :link="backpack_url('mailing')" />
